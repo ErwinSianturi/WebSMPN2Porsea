ee447c76e720e0745cc5aec38714a23f09539e23
Erwin Smith <149450533+ErwinSianturi@users.noreply.github.com>
2024-06-07 13:58:36 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
 
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <a href="/" class="h1"><b>SMP Negeri 2 PORSEA</a>
+                <a href="/" class="h1"><b>SMP Negeri 2 PORSEAA</a>
             </div>
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
